3749993962e82258004b887f89e4fcad0a2b9bdf
Byron-Hodel <byron@hodel.com>
2023-07-25 10:33:49 -0400

    added flag to optimize for speed

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-odin build src -out=insert_cool_name_here.wasm -target=js_wasm32 -debug -target-features:\"+bulk-memory,+atomics\"
+odin build src -o=speed -out=insert_cool_name_here.wasm -target=js_wasm32 -debug -target-features:\"+bulk-memory,+atomics\"
